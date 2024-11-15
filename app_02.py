322390bf9bd85b56cecad2fbd1ee493b45d8a36a
Valmir Junior <valmirmoreirajunior@gmail.com>
2024-11-15 05:46:03 -0300

    Parte 02

--- app_02.py
+++ app_02.py
@@ -8,10 +8,23 @@ def fetch_page(url):
 def parse_page(html):
 	soup = BeautifulSoup(html, 'html.parser')
 	product_name = soup.find('h1', class_='ui-pdp-title').get_text()
-	print(product_name)
+	price: list = soup.find_all('span', class_='andes-money-amount__fraction')
+	old_price: int = int(price[0].get_text().replace('.', ''))
+	new_price: int = int(price[1].get_text().replace('.', ''))
+	installment_price: int = int(price[2].get_text().replace('.', ''))
+
+	return{
+		'product_name' : product_name,
+		'old_price' : old_price,
+		'new_price' : new_price,
+		'installment_price' : installment_price,
+		
+	}
+	
 
 if __name__ == "__main__":
 	url = "https://www.mercadolivre.com.br/apple-iphone-16-pro-max-1-tb-titnio-natural-distribuidor-autorizado/p/MLB1040287867?pdp_filters=item_id:MLB3846015269#wid=MLB3846015269&sid=search&is_advertising=true&searchVariation=MLB1040287867&position=5&search_layout=stack&type=pad&tracking_id=84de19cc-05ee-4ed7-b75f-1bb31ae86928&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=5&ad_click_id=YTRkOTdiYWYtZjEwZS00ODRiLTgyNGEtMDg2OWNmODkzNjRm"
 	page_content = fetch_page(url)
-	parse_page(page_content)
+	produto_info = parse_page(page_content)
+	print(produto_info)
 	
